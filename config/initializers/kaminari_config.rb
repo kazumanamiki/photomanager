f48d8f23064feb92ce1c0e5f023f95ce3ee33e44
kazumanamiki <kazuma.namiki@outlook.jp>
2015-03-16 17:14:32 +0900

    ・新規作成をナビゲーションに移動 ・インデックスの一覧表示を画像一覧形式に変更 ・その他いろいろ調整

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,10 +1,10 @@
 Kaminari.configure do |config|
-  config.default_per_page = 10
+  config.default_per_page = 25
   # config.max_per_page = nil
   # config.window = 5
   # config.outer_window = 5
-  # config.left = 0
-  # config.right = 0
+  config.left = 3
+  config.right = 3
   # config.page_method_name = :page
   # config.param_name = :page
 end
